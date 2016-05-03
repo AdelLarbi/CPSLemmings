88b86102e707a622e134a99aa8180728ecc8c2de
AdelLarbi <larbi.adel@yahoo.fr>
2016-05-03 16:59:44 +0200

    tests implem...

--- src/Lemmings/contractTests/AbstractLevelTest.java
+++ src/Lemmings/contractTests/AbstractLevelTest.java
@@ -22,7 +22,6 @@ public abstract class AbstractLevelTest {
 	
 	/**
 	 * Init --------------------------------------------------------------------
-	 * TODO ....
 	 */
 	@Test
 	public void level_Init_1() {
@@ -48,9 +47,33 @@ public abstract class AbstractLevelTest {
 		
 		level.init(x, y);
 		
+		boolean error = false;
+		
+		for (int j = 0; j < level.getHeight(); j++) {	
+			for (int i = 0; i < level.getWidth(); i++) {
+				if (i == 0 || j == level.getHeight() -1 || i == level.getWidth() -1 || j == 0) {
+					if (level.getNature(i, j) != Nature.METAL) {
+						error = true;
+						break;
+					}
+				} else {  
+					if (level.getNature(i, j) != Nature.EMPTY) {
+						error = true;
+						break;
+					}
+				}
+			}
+		}
+		
 		Assert.assertTrue("Test Level Init : ",
 				level.getHeight() ==  y
 			 && level.getWidth() ==  x
+			 && level.isEditing() ==  true
+			 && level.getXEntrance() == -1
+			 && level.getYEntrance() == -1
+			 && level.getXExit() == -1
+			 && level.getYExit() == -1 
+			 && !error
 		);
 	}
 	
@@ -59,7 +82,47 @@ public abstract class AbstractLevelTest {
 	 */
 	@Test
 	public void level_setNature_1() {
-		testLevelSetNatureWith(5, 10, Nature.DIRT);						 					
+		testLevelSetNatureWith(0, 0, Nature.DIRT);						 					
+	}
+	
+	@Test
+	public void level_setNature_2() {
+		testLevelSetNatureWith(0, 0, Nature.METAL);						 					
+	}
+	
+	@Test
+	public void level_setNature_3() {
+		testLevelSetNatureWith(0, 0, Nature.EMPTY);						 					
+	}
+	
+	@Test
+	public void level_setNature_4() {
+		testLevelSetNatureWith(99, 78, Nature.DIRT);						 					
+	}
+	
+	@Test
+	public void level_setNature_5() {
+		testLevelSetNatureWith(99, 78, Nature.METAL);						 					
+	}
+	
+	@Test
+	public void level_setNature_6() {
+		testLevelSetNatureWith(99, 78, Nature.EMPTY);						 					
+	}
+	
+	@Test
+	public void level_setNature_7() {
+		testLevelSetNatureWith(50, 50, Nature.DIRT);						 					
+	}
+	
+	@Test
+	public void level_setNature_8() {
+		testLevelSetNatureWith(50, 50, Nature.METAL);						 					
+	}
+	
+	@Test
+	public void level_setNature_9() {
+		testLevelSetNatureWith(50, 50, Nature.EMPTY);						 					
 	}
 	
 	private void testLevelSetNatureWith(int x, int y, Nature n) {			
@@ -71,15 +134,58 @@ public abstract class AbstractLevelTest {
 		int getYExit_atPre = level.getYExit();
 		
 		level.init(100, 80);
+		
 		level.setNature(x, y, n);		
 				
-		Assert.assertTrue("Test HotelVille Dépot : ",
-				level.getNature(x, y) == Nature.DIRT
+		Assert.assertTrue("Test Level SetNature : ",
+				level.getNature(x, y) == n
 			 &&	level.isEditing() == isEditing_atPre
 			 && level.getXEntrance() == getXEntrance_atPre
 			 && level.getYEntrance() == getYEntrance_atPre
 			 && level.getXExit() == getXExit_atPre
 			 && level.getYExit() == getYExit_atPre
+		);
+	}
+	
+	/**
+	 * goPlay ------------------------------------------------------------------
+	 */
+	@Test
+	public void level_getPlay_1() {
+		testLevelGoPlayWith(1, 42, 78, 55);						 					
+	}
+
+	private void testLevelGoPlayWith(int xEntrance, int yEntrance, int xExit, int yExit) {
+		
+		level.init(100, 80);
+		
+		for (int j = 0; j < level.getHeight(); j++) {	
+			for (int i = 0; i < level.getWidth(); i++) {
+				if (i == 0 || j == level.getHeight() -1 || i == level.getWidth() -1 || j == 0) {
+					level.setNature(i, j, Nature.METAL);
+				}
+			}
+		}
+		
+		level.goEditing();
+			
+		level.setNature(1, 41, Nature.EMPTY);
+		level.setNature(1, 42, Nature.EMPTY);
+		level.setNature(1, 43, Nature.EMPTY);
+		
+		level.setNature(78, 54, Nature.EMPTY);
+		level.setNature(78, 55, Nature.EMPTY);
+		level.setNature(78, 56, Nature.EMPTY);
+		
+		level.goPlay(xEntrance, yEntrance, xExit, yExit);
+		
+		Assert.assertTrue("Test Level GoPlay : ",
+				level.getNature(x, y) == n // FIXME
+			 &&	level.isEditing() == false
+			 && level.getXEntrance() == xEntrance
+			 && level.getYEntrance() == yEntrance
+			 && level.getXExit() == xEntrance
+			 && level.getYExit() == yEntrance
 		);
 	}
 }
--- src/Lemmings/contractTests/TestContractLevel.java
+++ src/Lemmings/contractTests/TestContractLevel.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import Lemmings.contract.LevelContract;
 import Lemmings.error.PreConditionError;
 import Lemmings.implementations.LevelImpl;
+import Lemmings.tools.Nature;
 
 public class TestContractLevel extends AbstractLevelTest {
 
@@ -17,16 +18,112 @@ public class TestContractLevel extends AbstractLevelTest {
 
 	/**
 	 * Init --------------------------------------------------------------------
-	 * @param x
-	 * @param y
+	 * @param width
+	 * @param height
 	 */	
 	@Test(expected=PreConditionError.class)
 	public void level_Init_5() {						
-		level.init(-5, 80);		
+		level.init(4, 80);		
 	}
 	
 	@Test(expected=PreConditionError.class)
 	public void level_Init_6() {						
-		level.init(20, 0);		
+		level.init(20, 4);		
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_Init_7() {						
+		level.init(-1, 80);		
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_Init_8() {						
+		level.init(20, -3);		
+	}
+	
+	/**
+	 * setNature ---------------------------------------------------------------
+	 * @param x
+	 * @param y
+	 * @param nature
+	 */	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_10() {						
+		level.setNature(-1, 50, Nature.DIRT);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_11() {						
+		level.setNature(50, -1, Nature.DIRT);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_12() {						
+		level.setNature(-99, 50, Nature.DIRT);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_13() {						
+		level.setNature(50, -99, Nature.DIRT);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_14() {						
+		level.setNature(100, 50, Nature.DIRT);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_15() {						
+		level.setNature(50, 80, Nature.DIRT);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_16() {						
+		level.setNature(999, 50, Nature.DIRT);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_17() {						
+		level.setNature(50, 999, Nature.DIRT);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_18() {						
+		level.setNature(-1, 50, Nature.METAL);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_19() {						
+		level.setNature(50, -1, Nature.METAL);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_20() {						
+		level.setNature(-99, 50, Nature.METAL);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_21() {						
+		level.setNature(50, -99, Nature.METAL);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_22() {						
+		level.setNature(100, 50, Nature.METAL);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_23() {						
+		level.setNature(50, 80, Nature.METAL);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_24() {						
+		level.setNature(999, 50, Nature.METAL);	
+	}
+	
+	@Test(expected=PreConditionError.class)
+	public void level_SetNature_25() {						
+		level.setNature(50, 999, Nature.METAL);	
 	}
 }
